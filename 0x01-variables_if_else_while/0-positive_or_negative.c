7e792c27f7fdb4dda59315821eafc70746b17643
Ibrahim-nassih <binni3673@gmail.com>
2023-08-14 16:24:10 -0700

    second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/* betty style doc for function main goes there */
+/**
+ * main -Determines if number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
