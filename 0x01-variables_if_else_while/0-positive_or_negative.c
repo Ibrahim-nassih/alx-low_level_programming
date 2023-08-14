7fcadb32b1362d64b592a77d443a8495a5c34c06
Ibrahim-nassih <binni3673@gmail.com>
2023-08-14 16:30:29 -0700

    second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* Print the generated number */
 printf("%d ", n);
-
 /* Check whether the number is positive, zero, or negative */
-if (n > 0) {
+if (n > 0) 
+{
 printf("is positive\n");
-} else if (n == 0) {
+} 
+else if (n == 0) 
+{
 printf("is zero\n");
-} else {
+} 
+else 
+{
 printf("is negative\n");
-}
-    
+}    
 return (0);
 }
 
