2861cb9b22637e3fdc70ec2c7999d6087bfffd79
Ibrahim-nassih <binni3673@gmail.com>
2023-08-14 18:16:14 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-
-int main() {
-    // Print the alphabet excluding 'q' and 'e' using putchar
-    char letter = 'a';
-    
-    while (letter <= 'z') {
-        if (letter != 'q' && letter != 'e') {
-            putchar(letter);
-        }
-        letter++;
-    }
-    
-    putchar('\n');  // Print a new line
-    
-    return 0;
+/**
+ * main - Print the alphabet excluding
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Print the alphabet excluding 'q' and 'e' using putchar*/
+char letter;    
+while (letter <= 'z')
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+letter++;
+}   
+putchar('\n');
+return (0);
 }
 
