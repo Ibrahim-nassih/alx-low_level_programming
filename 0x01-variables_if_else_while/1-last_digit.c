a0353a4f9ba6cc6b678fcec016feb0d44571d0e7
Ibrahim-nassih <binni3673@gmail.com>
2023-08-14 17:12:55 -0700

    last degit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main() {
-    // Seed the random number generator with the current time
-    srand(time(NULL));
-
-    // Generate a random number and store it in the variable 'n'
-    int n = rand();
-
-    // Get the last digit of the number
-    int lastDigit = n % 10;
-
-    // Print the last digit
-    printf("Last digit of %d is %d", n, lastDigit);
-
-    // Check the value of the last digit and print the appropriate message
-    if (lastDigit > 5) {
-        printf(" and is greater than 5\n");
-    } else if (lastDigit == 0) {
-        printf(" and is 0\n");
-    } else {
-        printf(" and is less than 6 and not 0\n");
-    }
-
-    return 0;
+/**
+ * main - print the last digit of the number stored in the variable n
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+int n;
+int lastDigit;
+/* Seed the random number generator with the current time*/
+srand(time(NULL));
+/* Generate a random number and store it in the variable 'n'*/
+n = rand();
+/* Get the last digit of the number*/
+lastDigit = n % 10;
+/* Print the last digit*/
+printf("Last digit of %d is %d", n, lastDigit);
+/* Check the value of the last digit and print the appropriate message*/
+if (lastDigit > 5) 
+{
+printf(" and is greater than 5\n");
+} 
+else if (lastDigit == 0) 
+{
+printf(" and is 0\n");
+}
+else
+{
+printf(" and is less than 6 and not 0\n");
+}
+return (0);
 }
 
