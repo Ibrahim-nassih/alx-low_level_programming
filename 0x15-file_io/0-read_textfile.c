2093d798cbc286d68a7cfca5202733c43931fe06
Ibrahim-nassih <binni3673@gmail.com>
2023-10-30 15:15:44 -0700

    C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,10 @@
 
 /**
  * read_textfile - reads text from a file and prints it
- * 0filename
+ * @filename: name of file to read
+ * @letters: number of bytes to read
  *
+ * Return: number bytes read/printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {       int fd;
